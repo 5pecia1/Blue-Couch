381972a0414406ada6acba6b0d10b35035f214cb
5pecia1 <pdpxpd@gmail.com>
2018-08-24 04:21:17 +0900

    add chart

--- View/chart.js
+++ View/chart.js
@@ -1,25 +1,53 @@
 google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(initChart);
 
-google.charts.setOnLoadCallback(drawChart);
+function initChart() {
+    console.log('init Chart!');
+    let chart = null;
+    let chartDataTable = null;
 
+    chartDataTable = new google.visualization.DataTable();
 
-function drawChart() {
-    const graphData = google.visualization.arrayToDataTable([
-        ['날짜', '기분'],
-        ['123', 123]
-    ]);
-    
-    var options = {
-      title: 'Company Performance',
-      curveType: 'function',
-      legend: { position: 'bottom' }
+    chartDataTable.addColumn('string', '날짜');
+    chartDataTable.addColumn('number', '글자 수');
+    chartDataTable.addColumn( {'type': 'string', 'role': 'style'} );
+
+    let options = {
+      title: '당신의 기분 변화',
+      legend: { position: 'bottom' },
+      vAxis: {
+        format: '0'
+      }
     };
-    
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    
-    chart.draw(graphData, options);
+
+    chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+
+    chart.draw(chartDataTable, options);
+    setChart().then((cursor) => {
+        cursor.onsuccess = (result) => {
+            let cursor = result.target.result;
+            if(cursor != null) {
+                console.log(cursor.value);
+                let color = cursor.value['Color'];
+                let date = cursor.value['Date'];
+                let content = cursor.value['Content'];
+                addRowsAndDraw(chart, chartDataTable, options, [date, content.length, 'point { fill-color:' + color + '}']);
+                cursor.continue();
+            }
+        }
+    });
 }
 
-function addDrawChart(rowData){
-    
+function setChart() {
+    return new Promise((resolve, reject) => {
+        let iDBObjectStore = db.transaction(['MemoTextField'],'readonly').objectStore('MemoTextField');
+        resolve(iDBObjectStore.openCursor());
+    });
+}
+
+function addRowsAndDraw(chart, chartDataTable, options, rows = []) {
+    chartDataTable.addRows([
+        rows
+    ]);
+    chart.draw(chartDataTable, options);
 }
